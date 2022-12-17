1278d6fc75d2a0f970c6ba56f2792c34bb45f674
Jakob Robinson <robi1456@umn.edu>
2022-12-17 12:37:07 -0600

    Added lerping piece-square tables

--- script.js
+++ script.js
@@ -3,103 +3,189 @@ var board,
 
 /*The "AI" part starts here */
 
-// returns move object, uses mini and maxi
-var minimax = function(depth, game, maxPlayer) {
+// returns move object when isTopLayer set to true
+
+var minimax = function( depth, game, alpha, beta, maxPlayer, isTopLayer ) {
+    if(!isTopLayer)
+    {
+        positionCount++;
+    }
+    
+    if ( depth == 0 ) return evaluateBoard(game.board());
 
     var bestMove;
+    var max = -Infinity;
+    var min = Infinity;
     var moves = game.ugly_moves();
-    if(maxPlayer)
-    {
-        if ( depth == 0 ) return evaluateBoard(game.board());
-        var max = -Infinity;
-        for (var m = 0; m < moves.length; m++) {
-            game.ugly_move(moves[m]);
-            var bestMoveScore = mini( depth - 1, game, -Infinity, Infinity );
+
+    for (var m = 0; m < moves.length; m++) {
+        var bestMoveScore = 0;
+
+        game.ugly_move(moves[m]);
+
+        // if(game.in_check() && !maxPlayer)
+        // {
+        //     checks++;
+        // }
+
+        if(game.game_over())
+        {
+            if(game.in_checkmate())
+            {
+                game.undo();
+
+                //console.log(maxPlayer + " checkmate detected");
+                //checkmates++;
+
+                if(isTopLayer)
+                {
+                    return moves[m];
+                }
+
+                if(maxPlayer)
+                {
+                    return Infinity;
+                }
+                else
+                {
+                    return -Infinity;
+                }
+            }
+            else
+            {
+                game.undo();
+            }
+        }
+        else
+        {
+            bestMoveScore += minimax( depth - 1, game, alpha, beta, !maxPlayer, false );
             game.undo();
-            if( bestMoveScore >= max )
+        }
+
+        //console.log(maxPlayer + " score: " + bestMoveScore);
+
+        if(maxPlayer)
+        {
+            if( isTopLayer && bestMoveScore >= max )
             {
                 max = bestMoveScore;
                 bestMove = moves[m];
             }
+
+            if( bestMoveScore > max )
+            {
+                max = bestMoveScore;
+            }  
+            
+            if( !isTopLayer && bestMoveScore > alpha )
+            {
+                alpha = bestMoveScore;
+            }
         }
-    }
-    else
-    {
-        if ( depth == 0 ) return evaluateBoard(game.board());
-        var min = Infinity;
-        for (var m = 0; m < moves.length; m++) {
-            game.ugly_move(moves[m]);
-            var bestMoveScore = maxi( depth - 1, game, -Infinity, Infinity );
-            game.undo();
-            if( bestMoveScore <= min )
+        else
+        {
+            if( isTopLayer && bestMoveScore <= min )
             {
                 min = bestMoveScore;
                 bestMove = moves[m];
             }
+
+            if( bestMoveScore < min )
+            {
+                min = bestMoveScore;
+            }
+
+            if( !isTopLayer && bestMoveScore < beta )
+            {
+                beta = bestMoveScore;
+            }
+        }
+        
+        if( beta <= alpha )
+        {
+            return bestMoveScore;
         }
     }
 
-    return bestMove;
-}
-
-var maxi = function( depth, game, alpha, beta ) {
-    positionCount++;
-    if ( depth == 0 ) return evaluateBoard(game.board());
-    var max = -Infinity;
-    var moves = game.ugly_moves();
-    for (var m = 0; m < moves.length; m++) {
-        game.ugly_move(moves[m]);
-        var bestMoveScore = mini( depth - 1, game, alpha, beta );
-        game.undo();
-
-        if( bestMoveScore > max )
-            max = bestMoveScore;
-        
-        if( bestMoveScore > alpha )
-            alpha = bestMoveScore;
-
-        if( beta <= alpha )
-            return bestMoveScore;
+    if(isTopLayer)
+    {
+        //console.log("Checkmates detected: " + checkmates);
+        //console.log("Black Checks detected: " + checks);
+        //console.log("White Checks detected: " + checks);
+        return bestMove;
     }
 
-    return max;
-}
-
-var mini = function ( depth, game, alpha, beta ) {
-    positionCount++;
-    if ( depth == 0 ) return evaluateBoard(game.board());
-    var min = Infinity;
-    var moves = game.ugly_moves();
-    for (var m = 0; m < moves.length; m++) {
-        game.ugly_move(moves[m]);
-        var bestMoveScore = maxi( depth - 1, game, alpha, beta );
-        game.undo();
-
-        if( bestMoveScore < min )
-            min = bestMoveScore;
-
-        if( bestMoveScore < beta )
-            beta = bestMoveScore;
-
-        if( beta <= alpha )
-            return bestMoveScore;
+    if(maxPlayer)
+    {
+        return max;
     }
-
-    return min;
+    else
+    {
+        return min;
+    }
 }
 
 var evaluateBoard = function (board) {
     var totalScore = 0;
     for (var i = 0; i < 8; i++) {
         for (var j = 0; j < 8; j++) {
-            totalScore = totalScore + getPieceValue(board[i][j]);
+            totalScore = totalScore + getPieceValue(board[i][j], i, j);
         }
     }
 
     return totalScore;
 }
 
-var getPieceValue = function (piece) {
+var newPhase = null
+var getPhase = function (board) {
+    var totalPhase = 636.0;
+    var phase = totalPhase
+    for (var i = 0; i < 8; i++) {
+        for (var j = 0; j < 8; j++) {
+            var piece = board[i][j];
+            if(piece == null)
+            {
+                phase -= 0;
+            }
+            else if(piece.type == 'p')
+            {
+                phase -= 10;
+            }
+            else if(piece.type == 'n')
+            {
+                phase -= 30;
+            }
+            else if(piece.type == 'b')
+            {
+                phase -= 30;
+            }
+            else if(piece.type == 'r')
+            {
+                phase -= 50;
+            }
+            else if(piece.type == 'q')
+            {
+                phase -= 90;
+            }
+        }
+    }
+
+    phase /= totalPhase;
+    for(var i = 0; i < 6; i++)
+    {
+        for (var y = 0; y < 8; y++)
+        {
+            for (var x = 0; x < 8; x++)
+            {
+                whiteTables[i][y][x] = (startWhiteTables[i][y][x] * (totalPhase - phase) + endWhiteTables[i][y][x] * phase)/totalPhase
+                blackTables[i][y][x] = (startBlackTables[i][y][x] * (totalPhase - phase) + endBlackTables[i][y][x] * phase)/totalPhase
+            }
+        }
+    }
+    
+}
+
+var getPieceValue = function (piece, x, y ) {
 
     if(piece == null)
     {
@@ -107,44 +193,52 @@ var getPieceValue = function (piece) {
     }
 
     var multi;
+    var isWhite;
+
+    var value;
 
     // to mult by -1 or not, w or b
 
     if(piece.color == 'w')
     {
         multi = 1;
+        isWhite = true;
     }
     else
     {
         multi = -1;
+        isWhite = false;
     }
 
     // p=pawn, n=knight, b=bishop, r=rook, q=queen, k=king
 
     if(piece.type == 'p')
     {
-        return 1 * multi;
+        value = 10 + ( isWhite ? whiteTables[0][y][x] :blackTables[0][y][x] );
     }
     else if(piece.type == 'n')
     {
-        return 3 * multi;
+        value = 30 + ( isWhite ? whiteTables[1][y][x] :blackTables[1][y][x] );
     }
     else if(piece.type == 'b')
     {
-        return 3 * multi;
+        value = 30 + ( isWhite ? whiteTables[2][y][x] :blackTables[2][y][x] )
     }
     else if(piece.type == 'r')
     {
-        return 5 * multi;
+        value = 50 + ( isWhite ? whiteTables[3][y][x] :blackTables[3][y][x] )
     }
     else if(piece.type == 'q')
     {
-        return 9 * multi;
+        value = 90 + ( isWhite ? whiteTables[4][y][x] :blackTables[4][y][x] )
     }
     else if(piece.type == 'k')
     {
-        return 500 * multi;
+        value = ( isWhite ? whiteTables[5][y][x] :blackTables[5][y][x] )
     }
+
+    return value * multi;
+
 }
 
 /* board visualization and games state handling */
@@ -162,24 +256,64 @@ var makeBestMove = function () {
     board.position(game.fen());
     renderMoveHistory(game.history());
     if (game.game_over()) {
-        alert('Game over');
+        gameOverMessage();
     }
 };
 
+var gameOverMessage = function()
+{
+    var string = 'Game over:'
+    if(game.half_moves >= 100)
+    {
+        string += ' Over 100 turns have passed';
+    }
+    if(game.in_checkmate())
+    {
+        string += ' Checkmate';
+    }
+    if(game.in_stalemate())
+    {
+        string += ' Stalemate';
+    }
+    if(game.in_draw())
+    {
+        string += ' Draw';
+    }
+    if(game.insufficient_material())
+    {
+        string += ' Insufficient material';
+    }
+    if(game.in_threefold_repetition())
+    {
+        string += ' Threefold repetition';
+    }
+
+    alert(string);
+}
+
 var positionCount;
+//var checkmates;
+//var checks;
 var getBestMove = function (game) {
     if (game.game_over()) {
-        alert('Game over');
+        gameOverMessage();
     }
 
     positionCount = 0;
+    //checkmates = 0;
+    //checks = 0;
     var depth = parseInt($('#search-depth').find(':selected').text());
 
     var d = new Date().getTime();
-    var bestMove = minimax(depth, game, false);
+    var bestMove = minimax(depth, game, -Infinity, Infinity, false, true );
     var d2 = new Date().getTime();
     var moveTime = (d2 - d);
     var positionsPerS = ( positionCount * 1000 / moveTime);
+
+    if(bestMove.captured)
+    {
+        getPhase(game.board());
+    }
 
     $('#position-count').text(positionCount);
     $('#time').text(moveTime/1000 + 's');
